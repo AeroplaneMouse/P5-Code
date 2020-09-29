9153298e4772bee2df759e4116b86738679b1a28
knaD123 <amad18@student.aau.dk>
2020-09-29 10:04:27 +0200

    Added weather file and opening of csv file

--- Main.py
+++ Main.py
@@ -1,2 +1,18 @@
 import sqlite3
+import csv
+
+# Opens csv file (replace directory)
+with open('/Users/Andre/Documents/GitHub/P5-Code/Weather.csv') as csvfile:
+    reader = csv.reader(csvfile, delimiter = ',')
+    for row in reader:
+        print(', '.join(row))
+
+# Creates database and empty table
 conn = sqlite3.connect(':memory:')
+
+cursor = conn.cursor()
+
+command_1 = """CREATE TABLE IF NOT EXISTS
+
+"""
+
