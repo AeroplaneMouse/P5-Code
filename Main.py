9b8d010b47a6697aaf650e5de6b209f5f42336e1
sekr18 <sekr18@student.aau.dk>
2020-10-05 14:44:05 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,18 +1,4 @@
 import sqlite3
 import csv
 
-# Opens csv file (replace directory)
-with open('datasets/weather.csv') as csvfile:
-    reader = csv.reader(csvfile, delimiter = ',')
-    for row in reader:
-        print(', '.join(row))
-
-# Creates database and empty table
-conn = sqlite3.connect(':memory:')
-
-cursor = conn.cursor()
-
-command_1 = """CREATE TABLE IF NOT EXISTS
-
-"""
 
