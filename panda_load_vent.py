f77ff3b134bcbf7dee1eca02801e7f205c243036
Daniel Fly <danielfly42@gmail.com>
2020-10-05 14:56:13 +0200

    WIP: Understanding Pandas

--- panda_load_vent.py
+++ panda_load_vent.py
@@ -1,15 +1,44 @@
 import pandas as pa
 
+path = 'datasets/%s.csv'
+datasetName = 'vent-minute-short'
 
-path = 'C:/Users/danie/OneDrive/AAU/P5/P5-Code/datasets/%s.csv'
-datasetName = 'vent-minute'
+
+# Create states
+def create_states():
+    states = {}
+    value = MIN_VALUE
+
+    while value < MAX_VALUE:
+        states[value] = str(value) + " -> " + str(value + INCREMENT)
+        value += INCREMENT
+
+    return states
+
+# Maps a value to a state
+def get_state(value, states):
+    interval = MIN_VALUE
+
+    while interval < value:
+        interval += INCREMENT
+    interval -= INCREMENT
+
+    return states[interval]
+
+
+
+
+
+
 
 # Create DataFrame
 vent = pa.read_csv(path % datasetName, header=0, sep=';')
 
 # Remove timestamp column from dataset and use as index
-# vent.index = pa.to_datetime(vent.pop('Timestamp'))
+vent.index = pa.to_datetime(vent.pop('Timestamp'))
 
-# tmp = vent.head(20)
+print(vent.Vent_HRVTempExhaustOut['2013-07-01 00:01:14-04:00'])
 
-print('tmp')
+
+
+print('Done')
