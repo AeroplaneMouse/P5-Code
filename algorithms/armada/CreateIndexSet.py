f08a25d6d0110831e2c21d4e59ecb73bd3052a14
knaD123 <amad18@student.aau.dk>
2020-10-15 13:48:45 +0200

    Update CreateIndexSet.py

--- algorithms/armada/CreateIndexSet.py
+++ algorithms/armada/CreateIndexSet.py
@@ -1,14 +1,16 @@
 from models.IndexSet import IndexSet
 from models.IndexRecord import IndexRecord
+import copy as copy
 
-def CreateIndexSet(stem, prefix, range_set, cs_list, IndexRecord):
-    for clientSequence in range_set.Records:
-        if range_set == None:
+# range set is an index set.
+def CreateIndexSet(stem, prefix, p_idx):
+    p_m_idx = copy.deepcopy(p_idx)
+    for cs in p_m_idx.Records:
+        if prefix == None:
             start_pos = 0
         else:
-            start_pos = IndexRecord.Pos
-        IndexRecord = start_pos + 1
-        for i, IndexRecord in range(cs_list):
-            if s in cs_list:
-                IndexSet.insert(cs_list[i], "a_intv", start_pos)
-    return IndexSet
+            start_pos = cs.pos
+        for i in range(start_pos+1, cs[-1]):
+            if stem in p_m_idx.Records:
+                p_m_idx.Records.append(IndexRecord[start_pos + i], IndexRecord.Interval, start_pos)
+    return p_m_idx
