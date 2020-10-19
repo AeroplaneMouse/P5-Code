1b6ff127a18518d1d44cd52aacde9ad5eb539391
Lassebk45 <49187473+Lassebk45@users.noreply.github.com>
2020-10-19 15:40:58 +0200

    Update CreatePattern.py

--- algorithms/armada/CreatePattern.py
+++ algorithms/armada/CreatePattern.py
@@ -1,29 +1,10 @@
 import numpy as np
 from models.TPattern import TPattern
 
-def CreatePattern(prefix, FState):
+def CreatePattern(prefix, stem):
 
+	dimension = np.shape(prefix)[0]
 
-
-    for i in range(np.shape(FState)[0]):
-
-        if (FState.end < prefix.start):
-            FState.state = "Before"
-        elif (FState.end > prefix.start && FState.end < prefix.end):
-            FState.state = "Overlaps"
-        elif (FState.end == prefix.start):
-            FState.state = "Begins"
-        elif (FState.end == prefix.end):
-            FState.state = "Ends"
-        elif (FState.start < prefix.start && FState.end > prefix.end):
-            FState.state = "Contains"
-
-
-    np.pad(prefix, ((0, 1), (0, 1)), mode='constant', constant_values=0)
-
-
-
-    array_size = prefix * FState.state
-    TPattern.matrix = np.arrange(array_size).reshape(prefix, FState.state)
+    newPattern = np.reshape(prefix, (dimension + 1, dimension + 1))
 
     return TPattern.matrix
