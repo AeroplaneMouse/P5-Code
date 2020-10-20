68f1470eeaf86bbab7b13de506945fcae82cee5a
Lassebk45 <49187473+Lassebk45@users.noreply.github.com>
2020-10-20 13:10:34 +0200

    Update CreatePattern.py

--- algorithms/armada/CreatePattern.py
+++ algorithms/armada/CreatePattern.py
@@ -6,20 +6,25 @@ from algorithms.armada.FindRelation import FindRelation
 
 def CreatePattern(prefix, stem):
 
-    temp = prefix
-    dimension = np.shape(temp)[0]
+    if prefix is None:
+        temp = np.ndarray((2, 2), dtype='object')
+        temp[0][1] = stem
+        temp[1][0] = stem
+        temp[1][1] = '='
+        return temp
 
-    if(dimension > 1):
-        temp = np.reshape(temp, (dimension + 1, dimension + 1))
+    elif prefix is np.ndarray:
+        dimension = np.shape(prefix)[0]
+        temp = np.reshape(prefix, (dimension+1, dimension+1))
+
         for i in range(dimension):
             temp[i+1][dimension] = FindRelation(temp[0][i+1], stem)
             temp[dimension][i+1] = '*'
 
+        temp[0][dimension] = stem
+        temp[dimension][0] = stem
+        temp[dimension][dimension] = '='
+        return temp
+
     else:
-        temp = np.ndarray((2, 2), dtype='object')
-
-    temp[0][dimension] = stem
-    temp[dimension][0] = stem
-    temp[dimension][dimension] = '='
-
-    return temp
+        return None
