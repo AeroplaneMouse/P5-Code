09425fd15838df5b318441f2163f588265fdb292
Daniel Fly <danielfly42@gmail.com>
2020-10-20 15:48:35 +0200

    Include main path to find dataset

--- Main.py
+++ Main.py
@@ -1,12 +1,14 @@
+import sys
 from preprocessors.vent import VentPreprocessor
 from findSupport import MakeStateSupportList, ComputeSupport
 from algorithms.armada.Armada import Armada
 
 
-def Main():
+def Main(mainPath):
     # Create preprocessor
+    path = mainPath + 'datasets/vent-minute-short.csv'
     vent = VentPreprocessor()
-    vent.InitializeDataFrame('datasets/vent-minute-short.csv', ';')
+    vent.InitializeDataFrame(path, ';')
 
     # Create CS and compute support
     cs = vent.GenerateTemporalDataFrame()
@@ -17,5 +19,10 @@ def Main():
     Armada(cs, supList).Run(0.7)
 
 
+
 if __name__ == '__main__':
-    Main()
+    # Change \ to /
+    path = sys.argv[0].replace('\\', '/')
+    # Remove Main.py
+    path = path[:-7]
+    Main(path)
