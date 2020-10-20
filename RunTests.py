a0d5aecbb76ec49afff1d166d5da5edd82aa9963
Daniel Fly <danielfly42@gmail.com>
2020-10-20 13:52:44 +0200

    Expanded runTests

--- RunTests.py
+++ RunTests.py
@@ -1,1 +1,18 @@
-from tests import CreatePattern_Tests
+import sys
+
+# Run all tests
+if len(sys.argv) == 1:
+    print('Running all tests')
+    from tests import CreatePattern_Tests
+
+# Run specific test
+else:
+    test = sys.argv[1]
+
+    if test == 'CreatePattern':
+        from tests import CreatePattern_Tests
+    elif test == '?':
+        # Another test
+        print()
+    else:
+        print('Unknown test: \'{}\''.format(test))
