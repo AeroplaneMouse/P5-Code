7362cfabbc43ae5a3bd22269da536972c5d3a8ad
rherum18 <44167651+rherum18@users.noreply.github.com>
2020-10-20 09:06:01 +0200

    KURT.exe (Noget med Tabs vs. Spaces)

--- algorithms/armada/CreatePattern.py
+++ algorithms/armada/CreatePattern.py
@@ -5,15 +5,15 @@ from algorithms.armada.FindRelation import FindRelation
 
 def CreatePattern(prefix, stem):
 
-	matrix = prefix.Matrix
-	dimension = np.shape(matrix)[0]
+    matrix = prefix.Matrix
+    dimension = np.shape(matrix)[0]
 
     newMatrix = np.reshape(matrix, (dimension + 1, dimension + 1))
 
     newMatrix[0][dimension] = stem.State
 
     for i in range(dimension):
-    	newMatrix[i+1][dimension] = FindRelation(newMatrix[0][i+1], stem)
+        newMatrix[i+1][dimension] = FindRelation(newMatrix[0][i+1], stem)
 
     newMatrix[dimension][dimension] = '='
 
