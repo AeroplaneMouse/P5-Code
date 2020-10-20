d5064012960c1ab36fb7d21f2d02e7fd0859669a
Daniel Fly <danielfly42@gmail.com>
2020-10-20 09:33:43 +0200

    Cleanup main

--- Main.py
+++ Main.py
@@ -2,17 +2,18 @@ from preprocessors.vent import VentPreprocessor
 from findSupport import MakeStateSupportList, ComputeSupport
 from algorithms.armada.Armada import Armada
 
-import numpy as np
 
 def Main():
     # Create preprocessor
     vent = VentPreprocessor()
     vent.InitializeDataFrame('datasets/vent-minute-short.csv', ';')
 
+    # Create CS and compute support
     cs = vent.GenerateTemporalDataFrame()
     supList = MakeStateSupportList(cs)
     ComputeSupport(cs, supList)
 
+    # Run Armada
     Armada(cs, supList).Run(0.7)
 
 
