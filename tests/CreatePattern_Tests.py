67a5c396cfdee083efe5b0ceca02c8f799d09470
Daniel Fly <danielfly42@gmail.com>
2020-10-20 12:38:54 +0200

    WIP: Testing

---
+++ tests/CreatePattern_Tests.py
@@ -0,0 +1,34 @@
+import pandas as pa
+from models.FState import FState
+from algorithms.armada.CreatePattern import CreatePattern
+from models.TPattern import TPattern
+
+
+def GenerateStates():
+    states = []
+
+    states.append(FState(
+        'A',
+        pa.to_datetime('2013-07-01 04:01:14'),
+        pa.to_datetime('2013-07-01 13:08:17')))
+
+    states.append(FState(
+        'B',
+        pa.to_datetime('2013-07-01 04:01:14'),
+        pa.to_datetime('2013-07-01 14:57:17')))
+
+    return states
+
+print('********************')
+print('Testing CreatePattern')
+print()
+
+print('1 | Generating states')
+states = GenerateStates()
+
+print('2 | Testing generation of 1-Patterns')
+emptyPrefix = TPattern(None)
+patternA = CreatePattern()
+
+
+print(states)
