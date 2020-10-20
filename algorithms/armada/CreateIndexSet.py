9a737b7233381799cb193165f24d1650b221a259
knaD123 <amad18@student.aau.dk>
2020-10-20 14:11:32 +0200

    Update CreateIndexSet.py

--- algorithms/armada/CreateIndexSet.py
+++ algorithms/armada/CreateIndexSet.py
@@ -1,5 +1,7 @@
+from typing import Pattern
 from models.IndexSet import IndexSet
 from models.IndexRecord import IndexRecord
+from models.FState import FState
 import copy as copy
 
 # range set is an index set.
@@ -12,5 +14,22 @@ def CreateIndexSet(stem, prefix, p_idx):
             start_pos = cs.pos
         for i in range(start_pos+1, cs[-1]):
             if stem in p_m_idx.Records:
+                IndexRecord.Interval = [stem.start, stem.end]
                 p_m_idx.Records.append(IndexRecord[start_pos + i], IndexRecord.Interval, start_pos)
     return p_m_idx
+
+# MDB is the list of client sequences
+def CreateFirstIndexSet(stem, MDB):
+    idx = IndexSet()
+    idx.Pattern = stem.Name
+    for cs in MDB:
+        start_pos = 0
+        for state in cs:
+            start_pos += 1
+            if state == stem:
+                state = stem.Name
+                intv = [stem.Start, stem.End]
+                idx.Records.append(state, intv, start_pos)
+    return idx
+
+
