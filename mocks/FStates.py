b4606df820ad1d1ba6e3b0f7b5837d5eb35fcbb9
Daniel Fly <danielfly42@gmail.com>
2020-10-21 01:17:26 +0200

    WIP: Testing - Updated mocks

--- mocks/FStates.py
+++ mocks/FStates.py
@@ -1,4 +1,6 @@
 from models.FState import FState
+import pandas as pa
+
 
 A = FState(
     'A',
@@ -14,5 +16,15 @@ C = FState(
     'C',
     10,
     22)
+
+D = FState(
+    '0_20->25',
+    pa.to_datetime('2013-07-01 04:01:14'),
+    pa.to_datetime('2013-07-01 13:08:17'))
+
+E = FState(
+    '1_20->25',
+    pa.to_datetime('2013-07-01 04:01:14'),
+    pa.to_datetime('2013-07-01 14:57:17'))
 
 All = [A, B, C]
