0a1331e5bdf79cfd2bb0e6d0c525833baa110289
Daniel Fly <danielfly42@gmail.com>
2020-10-21 11:45:45 +0200

    Added remove non-frequent states from MDB and supList

--- Main.py
+++ Main.py
@@ -3,6 +3,31 @@ from findSupport import MakeStateSupportList, ComputeSupport
 from algorithms.armada.Armada import Armada
 
 
+# Check if a state name is in suplist
+def Contains(state, supList):
+    for supState in supList:
+        if supState.StateName == state:
+            return True
+
+    return False
+
+
+def RemoveLowSup(minSup, supList, cs):
+    # Clear supList for states below minSup
+    for i in reversed(range(0, len(supList))):
+        if supList[i].Support < minSup:
+            del supList[i]
+
+    # Clear cs for states not in supList
+    for i in reversed(range(0, len(cs))):
+        singleCS = cs.iloc[i]
+
+        if not Contains(singleCS.State, supList):
+            cs = cs.drop(i)
+
+    return cs
+
+
 def Main():
     # Create preprocessor
     vent = VentPreprocessor()
@@ -12,11 +37,16 @@ def Main():
     cs = vent.GenerateTemporalDataFrame()
     supList = MakeStateSupportList(cs)
     ComputeSupport(cs, supList)
-    csList = SplitDataframe(cs)
-    
-    # Run Armada
-    Armada(csList, supList).Run(0.7)
 
+    minSup = 0.7
+    cs = RemoveLowSup(minSup, supList, cs)
+    mdb = SplitDataframe(cs)
+
+    # print(supList)
+    #Run Armada
+    Armada(mdb, supList).Run(minSup)
+
+    # print(cs)
 
 if __name__ == '__main__':
     Main()
