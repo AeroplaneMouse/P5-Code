bdc9e044f329245a966f34263b53daaff71abf88
knaD123 <44018494+knaD123@users.noreply.github.com>
2020-10-22 13:02:56 +0200

    Update CreateIndexSet.py

--- algorithms/armada/CreateIndexSet.py
+++ algorithms/armada/CreateIndexSet.py
@@ -5,19 +5,23 @@ from algorithms.armada.CreatePattern import CreatePattern
 from algorithms.armada import Storage
 import numpy as np
 
-
+# visited_states is used to keep check of frequent states that already have an index set
 # range set is an index set.
 def CreateIndexSet(stem, prefix, range_set, visited_states):
     p_m_idx = IndexSet(CreatePattern(prefix, stem), [])
+    #Checks if a index set is already created
     if stem.State not in visited_states:
         visited_states.append(stem.State)
+        #Goes through all the client sequences in range_set
         for cs in range_set:
             if range_set == Storage.MDB:
                 start_pos = 0
             else:
                 start_pos = range_set.pos
             pos = (start_pos + 1)
+            #goes through the frequent states in cs
             for pos in range(pos, len(cs)):
+                #if the state found is equal the index set pattern add to index set
                 if cs.iloc[pos].State == stem:
                     ref = cs
                     intv = [[cs.iloc[pos].Start, cs.iloc[pos].End]]
