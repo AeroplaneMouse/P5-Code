0e934c7dd1f34196c7591d69071ab62beba38e54
Lassebk45 <49187473+Lassebk45@users.noreply.github.com>
2020-11-11 11:22:41 +0100

    comments

--- algorithms/armada/CreatePattern.py
+++ algorithms/armada/CreatePattern.py
@@ -1,22 +1,24 @@
 import numpy as np
-# from models.TPattern import TPattern
-# from models.FState import FState
 from algorithms.armada.FindRelation import FindRelation
 
 
 def CreatePattern(prefix, stem):
+    #if there is no prefix pattern, create one-pattern from stem
     if prefix is None:
         temp = np.ndarray((2, 2), dtype='object')
         temp[0][1] = stem
         temp[1][0] = stem
         temp[1][1] = '='
         return temp
-
+    
+    #append prefix
     else:
+
         dimension = np.shape(prefix)[0]
         temp = np.concatenate((prefix, np.zeros((1, dimension), dtype='object')))
         temp = np.concatenate((temp, np.zeros((dimension+1, 1), dtype='object')), axis=1)
 
+        #compute relations for each cell
         for i in range(dimension-1):
             temp[i+1][dimension] = FindRelation(temp[0][i+1], stem)
             temp[dimension][i+1] = '*'
