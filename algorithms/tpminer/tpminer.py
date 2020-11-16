64037baaeeca28a457d01f9429d031be9c15a553
sekr18 <sekr18@student.aau.dk>
2020-11-16 15:23:28 +0100

    Update tpminer.py

--- algorithms/tpminer/tpminer.py
+++ algorithms/tpminer/tpminer.py
@@ -5,10 +5,19 @@ def TDBToEndpointSequenceList(mdb):
     for element in mdb:
         EndpointSequence = []
         
-        for row in element.iterrows():
-            
+        i = element.at[0,'Start']
+        j = element.at[0,'End']
         
+        while (i<len(element)):
+            if element.at[j,'End'] < element.at[i,'Start']:
+                # create a finishing endpoint for j, and append to Endpoint Sequence.
+                # count up j
+            else:
+                # Create a starting endpoint for i, and append to Endpoint Sequence.
+                # count up i
         
+        while (j<len(element)):
+            # create remaining endpoints for finishing endpoints
         
         EndpointSequenceList.Append(EndpointSequence)
         
