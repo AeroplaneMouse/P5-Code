833119d7fb12c6f8e81de2fd50757dbb07b3b554
Lassebk45 <49187473+Lassebk45@users.noreply.github.com>
2020-11-16 16:01:36 +0100

    Update Endpoint.py

--- tpmmodels/Endpoint.py
+++ tpmmodels/Endpoint.py
@@ -1,9 +1,11 @@
 class Endpoint:
-    def __init__(self, label, isStart, parentes):
+    def __init__(self, label, isStart, parenthesis):
         self.Label = label
         self.OccurNum = 0 
         self.IsStart = isStart
-        self.Parentes = parentes
+        self.Parenthesis = parenthesis
+        self.Prune = false
+
 
     def __str__(self):
         return self.Label
