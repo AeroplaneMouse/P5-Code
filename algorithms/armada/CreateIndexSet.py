4c79f3a5739c55b1f1835bdf309dcf384d6b1233
Daniel Fly <danielfly42@gmail.com>
2020-11-19 10:31:32 +0100

    CreateIndexSet.py cleanup

--- algorithms/armada/CreateIndexSet.py
+++ algorithms/armada/CreateIndexSet.py
@@ -3,7 +3,7 @@ from models.IndexRecord import IndexRecord
 from algorithms.armada import Storage
 
 
-# range set is an index set.
+# Range set is an index set.
 def CreateIndexSet(stem, pattern, range_set):
     p_m_idx = IndexSet(pattern, [])
 
@@ -41,39 +41,3 @@ def CreateIndexSet(stem, pattern, range_set):
                 break
 
     return p_m_idx
-
-
-def printPatterns():
-    # for i in range(len(patterns)):
-    #     print("Pattern nr: " + str(i))
-    #     print(patterns[i])
-    #     print()
-    print('Patterns: {}'.format(len(patterns)))
-
-
-"""
-# MDB is the list of client sequences
-# visited_states is used to keep check of frequent states that already have an index set
-def CreateFirstIndexSet(stem, visited_states):
-    idx = IndexSet(CreatePattern(None, stem.State), [])
-    #Checks if a index set is already created
-    if stem.State not in visited_states:
-        visited_states.append(stem.State)
-        #Goes through all the client sequences in mdb
-        for cs in Storage.MDB:
-            #goes through the frequent states in cs
-            for singleState in range(0,len(cs)):
-                #if the state found is equal the index set pattern add to index set
-                if cs.iloc[singleState].State == stem.State:
-                    
-                    ref = cs
-                    intv = [[cs.iloc[singleState].Start, cs.iloc[singleState].End]]
-                    pos = singleState
-
-                    new_rec = IndexRecord(pos, intv, ref)
-
-                    idx.Records.append(new_rec)
-                    break
-            continue
-        return idx
-"""
