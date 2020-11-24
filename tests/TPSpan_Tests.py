a57730c72491ed928557e67e2c245e60ea13be7d
Lassebk45 <49187473+Lassebk45@users.noreply.github.com>
2020-11-24 14:12:21 +0100

    yes

--- tests/TPSpan_Tests.py
+++ tests/TPSpan_Tests.py
@@ -5,14 +5,12 @@ from algorithms.tpminer.TPSpan import TPSpan
 def test_TPSpan():
     FE = {a_s, b_s, c_s}
     TP = set()
-    l = [[a_s, b_s, b_f, a_f], 
-                    [a_s, b_s, b_f, a_f, c_s, c_f], 
-                    [b_s, b_f],
-                    [b_s, b_f, c_s, c_f],
-                    [c_s, c_f]]
+    l = [[a_s, b_s, b_f, a_f], [a_s, a_f]]
     
     expected_result = set(tuple(i) for i in l)
     TPSpan([a_s], db, 0.5, TP, FE)
+    print(expected_result)
+    print(TP)
     t.test(TP == expected_result, 'TPSpan Test #1')
 
 test_TPSpan()
