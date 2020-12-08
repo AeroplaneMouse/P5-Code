eb21604d19e457a513c715d80a2f7d42df93b6c1
Lassebk45 <49187473+Lassebk45@users.noreply.github.com>
2020-12-08 13:46:42 +0100

    some dumbass changes tbh dont @ me

--- algorithms/tpminer/point_pruning.py
+++ algorithms/tpminer/point_pruning.py
@@ -1,15 +1,16 @@
 from algorithms.tpminer.remove_corresponding_eps import remove_corresponding_eps
 
 
+#def point_pruning(FE, a, lone_eps):
 def point_pruning(FE, a):
-	
-	prfx_starting_ep = remove_corresponding_eps(a)
 
 	temp_points = set()
 
+	lone_eps = remove_corresponding_eps(a)
+
 	for s in FE:
 		if s.IsStart == False:
-			for s_ep in prfx_starting_ep:
+			for s_ep in lone_eps:
 				if s.Label == s_ep.Label and s_ep.IsStart == True:
 					temp_points.add(s)
 					break
--- algorithms/tpminer/tpminer_main.py
+++ algorithms/tpminer/tpminer_main.py
@@ -28,9 +28,13 @@ def tpminer_main(mdb, min_sup, logger):
     n = len(FE)
 
     for s in FE:
-        db_s = db_construct(db, [s])
+        #db_s = db_construct(db, [s], [s])
+        db_pruned, db_s = db_construct(db, [s])
 
-        TPSpan([s], db_s, min_sup, TP)
+        print(s)
+
+        #TPSpan([s], db_s, min_sup, TP, [s])
+        TPSpan([s], db_s, min_sup, TP, db_pruned)
 
         m = 'TPMiner {:0.1f}%'.format((i/n)*100)
         log = Log(m, Severity.INFO)
@@ -38,6 +42,8 @@ def tpminer_main(mdb, min_sup, logger):
 
         i += 1
 
+    for ptn in TP:
+        print(ptn)
     return TP
 
 
@@ -86,3 +92,23 @@ def convert_to_db(mdb):
         i += 1
 
     return temp
+
+def validate_data(data):
+    for l in data:
+        i = 1
+        for ep in l:
+            if ep.IsStart:
+                label = ep.Label
+                ep.Foo = True
+                for epp in l[i:]:
+                    if epp.Label == label and not epp.IsStart and not epp.Foo:
+                        epp.Foo = True
+                        break
+            i += 1
+
+    for l in data:
+        for ep in l:
+            if not ep.Foo:
+                print("fail")
+                print(l)
+
