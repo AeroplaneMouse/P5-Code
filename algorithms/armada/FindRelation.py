a0db1d53a49312d32b04dbbf03742b2093b7f23e
Daniel Fly <danielfly42@gmail.com>
2020-12-20 03:16:15 +0100

    Added comment to FindRelation

--- algorithms/armada/FindRelation.py
+++ algorithms/armada/FindRelation.py
@@ -1,19 +1,33 @@
 def FindRelation(record1, record2):
     if(record1.Start < record2.Start):
+        # Before
         if (record1.End < record2.Start):
             return 'b'
+
+        # Meets
         elif (record1.End == record2.Start):
             return 'm'
+
+        # Overlaps
         elif(record1.End > record2.Start and record1.End < record2.End):
             return 'o'
+
+        # Finishes
         elif(record1.End == record2.End):
             return 'f'
+
+        # Contains
         elif(record1.End > record2.End):
             return 'c'
+
     elif(record1.Start == record2.Start):
+        # Equals
         if(record1.End == record2.End):
             return 'e'
+
+        # Starts
         elif(record1.End < record2.End):
             return 's'
     else:
+        # Error
         return "X"
